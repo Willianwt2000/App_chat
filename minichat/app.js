c3d192180565c890ee386bba62597f686d7b9bd9
willianwt2000 <willianmetelien@gmail.com>
2025-02-24 01:12:21 -0400

    agregando metadatos y corrigiendo bugs

--- minichat/app.js
+++ minichat/app.js
@@ -8,20 +8,34 @@ const io = new Server(server);
 // Middleware para servir archivos estáticos
 app.use(express.static(__dirname));
 
+// Manejar conexiones de Socket.IO
 io.on("connection", (socket) => {
+  console.log("Un usuario se ha conectado");
 
+  // Escuchar mensajes del cliente
   socket.on('chat message', (msg) => {
-    console.log(`Mensaje: ${msg}`)
+    console.log(`Mensaje recibido: ${msg}`);
 
-    io.emit('chat message',msg)
-  })
+    // Reenviar el mensaje a todos los clientes (incluido el que lo envió)
+    io.emit('chat message', {
+      text: msg,
+      sender: socket.id, // Enviar el ID del remitente
+    });
+  });
+
+  // Manejar desconexiones
+  socket.on('disconnect', () => {
+    console.log("Un usuario se ha desconectado");
+  });
 });
+
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
